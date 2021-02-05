1261d5250789863eb01478ce4d7e8aed68447a20
rzthrun <rzthrun@gmail.com>
2021-02-05 10:33:34 -0500

    multi-program build to force deploybot

--- dist/modules/modules.0c3a35b4.js
+++ dist/modules/modules.0c3a35b4.js
@@ -156,7 +156,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'multiprogram',apiUrl:'https://multi.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://multiprogram.fielddoc.org',clientId:'iwN6fjRffe1RUmSfiJL3FPZA7WVwrA8B',version:1612538914643})
+.constant('environment', {name:'multiprogram',apiUrl:'https://multi.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://multiprogram.fielddoc.org',clientId:'iwN6fjRffe1RUmSfiJL3FPZA7WVwrA8B',version:1612539151583})
 
 ;
 /**
