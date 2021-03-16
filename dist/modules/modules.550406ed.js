620467b5d5514eedfa43b58bce762a39d47d936c
brendanmcintyre <brendan.david.mcintyre@gmail.com>
2021-03-16 02:29:21 -0400

    Restored batch upload directive to project and site summary views (production build included)

--- dist/modules/modules.550406ed.js
+++ dist/modules/modules.550406ed.js
@@ -157,7 +157,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',authDeferralKey:'qu8TTMdvJH1mrx6Zu6pbbwPGM0ULeoKb',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1615875710250})
+.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',authDeferralKey:'qu8TTMdvJH1mrx6Zu6pbbwPGM0ULeoKb',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1615876031279})
 
 ;
 /**
