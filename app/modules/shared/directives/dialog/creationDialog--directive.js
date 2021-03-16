68d5aa647d91c9e8a1f52b6bce66d25d2e56a204
rzthrun <rzthrun@gmail.com>
2021-03-16 10:56:50 -0400

    unnecessary directive param var removed

--- app/modules/shared/directives/dialog/creationDialog--directive.js
+++ app/modules/shared/directives/dialog/creationDialog--directive.js
@@ -31,7 +31,7 @@
                         'type': '=?',
                         'visible': '=?',
                         'availableprograms' : '=?',
-                        'programs': '=?',
+
                     },
                     templateUrl: function(elem, attrs) {
 
